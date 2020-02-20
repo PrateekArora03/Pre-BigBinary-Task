5ee054c41a64cc235d84ae5711b1a1510fd5995a
prateekcodes <prateekisthebest98@gmail.com>
2020-02-20 22:16:14 +0530

    add: useReducer for github-label

---
+++ feature/Github-Label/client/src/context/label/labelReducer.js
@@ -0,0 +1,43 @@
+import {
+  ADD_LABEL,
+  GET_LABELS,
+  UPDATE_LABEL,
+  DELETE_LABEL,
+  TOGGLE_NEW_LABEL
+} from "../types";
+
+export default (state, action) => {
+  switch (action.type) {
+    case TOGGLE_NEW_LABEL:
+      return {
+        ...state,
+        toggle: !state.toggle
+      };
+    case GET_LABELS:
+      return {
+        ...state,
+        labels: action.payload
+      };
+    case ADD_LABEL:
+      return {
+        ...state,
+        labels: [...state.labels, action.payload]
+      };
+    case UPDATE_LABEL:
+      return {
+        ...state,
+        labels: state.labels.map(label => {
+          if (label._id === action.payload._id) {
+            label = action.payload;
+          }
+        })
+      };
+    case DELETE_LABEL:
+      return {
+        ...state,
+        labels: state.labels.filter(label => label._id !== action.payload)
+      };
+    default:
+      return state;
+  }
+};
